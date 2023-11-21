ac696f433146d7ca9b669d6c2ae65877c5a570d2
VnandanS <vidyanandan.s@moviuscorp.com>
2023-11-21 14:32:52 +0530

    Integrate semantic UI

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "jquery"; // this import first
+
+//= require semantic-ui
+
+$(document).ready(function(){ $('.ui.dropdown').menu()});
+
+
+// $(document).on('load', function(){
+//   $('.ui.dropdown').dropdown();
+// });
